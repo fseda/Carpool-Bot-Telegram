571144f9da6d428eb867a804395a8a763844ecdb
fseda <63421531+fseda@users.noreply.github.com>
2023-06-28 02:17:26 +0000

    Add two more commands (help and about)

--- main.py
+++ main.py
@@ -141,11 +141,59 @@ def set_ride_to_full(message):
                                           telegram_username=username,
                                           carpool_type=carpool_type).first()
 
+  if not ride:
+    return
+
   ride.available_seats = 0
   ride.save()
 
   return tb.send_message(
     chat_id, f'@{username} atualizou o número de vagas de {direction} para 0')
+
+
+@tb.message_handler(commands=['help'])
+def help(message):
+  chat_id = message.chat.id
+
+  return tb.send_message(
+    chat_id,
+    f'''Utilize esse Bot para agendar as caronas. A utilização é super simples e através de comandos:
+        /caronas --> Esse comando lista as caronas tanto de ida, quanto de volta do Fundão
+        
+        /ida [horario] [vagas] [local] --> Esse comando serve para definir um horário que você está INDO para o Fundão.
+         Ex: /ida 11:00 3 Barra Shopping
+         (Inclui uma carona de ida às 11:00 com 3 vagas saindo do Barra Shopping)
+
+        /ida --> Esse comando lista todas as caronas de IDA disponíveis
+
+        /volta [horario] [vagas] [local] --> Esse comando serve para definir um horário que você está VOLTANDO para o SEU BAIRRO. 
+         Ex: /volta 15:00 1 Posto 5 
+         (Inclui uma carona de volta às 15:00 com 1 vaga para o Posto 5)
+        
+        /volta --> Esse comando lista todas as caronas de VOLTA disponíveis
+
+        OBS --> O local precisa ser composto por no MÁXIMO duas palavras. Para mais de um local siga o padrão : local01/local02 
+         Ex: gramado/macembu/mananciais/guerenguê/rodrigues caldas
+
+        /remover [ida|volta] --> Comando utilizado para remover a carona da lista. SEMPRE REMOVA a carona depois dela ter sido realizada. 
+         Ex: /remover ida
+
+        /vagas [ida|volta] [vagas] --> Esse comando serve para atualizar o número de vagas de uma carona
+         Ex: /vagas ida 2 
+         (Altera o número de vagas da ida para 2)
+         Ex: /vagas ida 0
+         (Altera o número de vagas da ida para 0, ou seja, lotado)
+         
+         /lotou --> Esse comando serve para atualizar o número de vagas para 0. (Lotado)'''
+  )
+
+
+@tb.message_handler(commands=['about'])
+def about(message):
+  chat_id = message.chat.id
+
+  return tb.send_message(chat_id,
+                         "https://github.com/fseda/carpool-bot-telegram")
 
 
 keep_alive()
